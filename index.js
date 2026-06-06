996888b0ff082a4e36b024a922306f819ed8eaab
victoriawanko-create. <victoriawanko@gmail.com.>
2026-06-06 18:49:28 -0400

    6:49 pm 6-6-26

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
-async function main {
+async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
     const userListEl = document.querySelector(".user-list");
 
-    userListEl.innerHTML = usersData
-    .map(
-        (user) =>`<div class="user-card">
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
+
+main();
+
+function showUserPosts(id) {
+    localStorage.setItem("id", id);
+    window.location.href = `${window.location.origin}/user.html`;
+    console.log(id);
+}
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
             <div class="user-card__container">
                 <h3>${user.name}</h4>
                     <p><b>Email:</b> ${user.email}</p>
                     <p><b>Phone:</b> ${user.phone}</p>
-                    <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+                    <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
                 </div>
-            </div>`
-    )
-    .join("");
-}
-
-main();
+            </div>`;
+    }
