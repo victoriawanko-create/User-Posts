ec8784ad2ad2b9e3bffbbec163f364d20f3b6651
victoriawanko-create. <victoriawanko@gmail.com.>
2026-06-07 14:08:24 -0400

    final draft

--- user.js
+++ user.js
@@ -1,8 +1,28 @@
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
-    const postsData = await posts.json();
-    console.log(postsData);
+const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id")
+
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
 }
 
-main();
+async function renderPosts(userId) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`);
+    const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map(post => postsHTML(post)).join('');
+}
+
+function postsHTML(post) {
+    return `
+        <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>
+    `
+}
+
+renderPosts(id);
